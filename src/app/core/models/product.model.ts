52bc8e55fc3cec68e1c9a78165850ca75d58ae5f
oberroteran <oberroteran@materiagris.pe>
2021-07-10 14:15:48 -0500

    init

---
+++ src/app/core/models/product.model.ts
@@ -0,0 +1,17 @@
+export interface Product {
+  // id: string;
+  // title: string;
+  // price: number;
+  // description: string;
+  // image: string;
+
+
+  id: string;
+  stock: number;
+  precio: number;
+  marca: string;
+  descripcion: string;
+  categoriaId: number;
+  img: string;
+
+}
---
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { ProductsService } from './../../../core/services/products/products.service';
+import { Product } from './../../../core/models/product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  product: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params.id;
+      this.fetchProduct(id);
+      // this.product = this.productsService.getProduct(id);
+    });
+  }
+
+  fetchProduct(id: string) {
+    this.productsService.getProduct(id)
+    .subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  createProduct() {
+    const newProduct: Product = {
+      id: '222',
+      descripcion: 'nuevo desde angular',
+      img: 'assets/images/banner-1.jpg',
+      precio: 3000,
+      marca: 'nuevo producto',
+      stock: 0,
+      categoriaId: 0
+    };
+    this.productsService.createProduct(newProduct)
+    .subscribe(product => {
+      console.log(product);
+    });
+  }
+
+  updateProduct() {
+    const updateProduct: Partial<Product> = {
+      precio: 555555,
+      descripcion: 'edicion titulo'
+    };
+    this.productsService.updateProduct('2', updateProduct)
+    .subscribe(product => {
+      console.log(product);
+    });
+  }
+
+  deleteProduct() {
+    this.productsService.deleteProduct('222')
+    .subscribe(rta => {
+      console.log(rta);
+    });
+  }
+
+}
